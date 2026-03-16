152177f7aa413e1097841553258977144012c512
efromvt <ethan.dickinson@gmail.com.com>
2026-03-16 09:41:59 -0400

    better CI report

--- src/playwright.config.ts
+++ src/playwright.config.ts
@@ -6,17 +6,29 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? 'github' : 'list',
+
+  reporter: process.env.CI
+    ? [
+        ['github'],
+        ['html', { outputFolder: 'playwright-report', open: 'never' }],
+      ]
+    : [
+        ['list'],
+        ['html', { open: 'on-failure' }],
+      ],
+
   use: {
     baseURL: 'http://localhost:4173',
     trace: 'on-first-retry',
   },
+
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
   ],
+
   webServer: {
     command: 'pnpm preview --port 4173',
     port: 4173,
